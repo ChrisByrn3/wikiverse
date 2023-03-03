dfc9f31703332a3821ed4101c35a388f6040d18e
ChrisByrn3 <chris.byrn3@gmail.com>
2023-03-03 15:38:16 +0000

    Attempt at creating list links

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { PagesList } from './PagesList';
+import {Page} from './page';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
@@ -7,6 +8,7 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [pages, setPages] = useState([]);
+	const [details, setDetails] = useState(null);
 
 	async function fetchPages(){
 		try {
@@ -22,11 +24,29 @@ export const App = () => {
 		fetchPages();
 	}, []);
 
+	async function fetchDetails(slug) {
+		try {
+            const response = await fetch(`${apiURL}/wiki/${slug}`);
+            const pageData = await response.json();
+            setDetails(pageData);
+        } catch (err) {
+            console.log("Oh no an error! ", err)
+        }
+    }
+
 	return (
 		<main>	
       <h1>WikiVerse</h1>
+	  {details ? (
+		<Page article={details} 
+		onBackClick ={() => setDetails(null)}/>
+	  ) : (
+		<>
 			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
+			<PagesList pages={pages} 
+			onArticleClick={fetchDetails}/>
+			</>
+	  )}
 		</main>
 	)
 }
--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export const Page = (props) => {
+export const Page = ({details, onBackClick}) => {
 
   return <>
-    <h3>{props.page.title}</h3>
+    <h3>{details.title}</h3>
+    <p>{details.author}</p>
+    <p>{details.content}</p>
+    <p>{details.tags}</p>
+    <p>{details.date}</p>
+
+    <button onClick={onBackClick}>Home</button>
   </>
 } 
 	
